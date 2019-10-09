474b041c5c940a58e706c03b49ff7d791fda4128
Matix97 <46028745+Matix97@users.noreply.github.com>
2019-10-09 20:16:33 +0200

    coś już się dzieje gdy się wpisze text

--- src/Entry.tsx
+++ src/Entry.tsx
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export class Entry extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
   render() {
     return (
       <View style={styles.app}>
         <Text style={styles.text}>{this.props.name}</Text>
         <TextInput
           style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          onChangeText={(text) => {this.setState({text});console.log(text)}}
+          //value={this.state.text}
         />
       </View>
     );
