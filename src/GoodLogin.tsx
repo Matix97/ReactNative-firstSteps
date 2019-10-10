71d37aceea78464d5e24ed1a380658b5adbeab61
Matix97 <46028745+Matix97@users.noreply.github.com>
2019-10-10 11:41:44 +0200

    bad and good

---
+++ src/GoodLogin.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export class GoodLogin extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Dobry Login</Text>
+      </View>
+    );
+  }
+}
